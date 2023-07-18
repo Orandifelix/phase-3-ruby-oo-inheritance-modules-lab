b4b616e3d4157da27a1da88e02e00ffa14f8bd23
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-07-18 09:19:34 +0300

    Tried my best

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,65 @@
-require 'pry'
+# require 'pry'
+
+# class Artist
+#   attr_accessor :name
+#   attr_reader :songs
+
+#   @@artists = []
+
+#   def initialize
+#     @@artists << self
+#     @songs = []
+#   end
+
+#   def self.find_by_name(name)
+#     @@artists.detect{|a| a.name == name}
+#   end
+
+#   def self.all
+#     @@artists
+#   end
+
+#   def self.reset_all
+#     self.all.clear
+#   end
+
+#   def self.count
+#     self.all.count
+#   end
+
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
+
+#   def add_songs(songs)
+#     songs.each { |song| add_song(song) }
+#   end
+
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
+
+
+
+require_relative '../concerns/memorable'
+require_relative '../concerns/paramable'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable
+  include Memorable::InstanceMethods
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -32,11 +67,8 @@ class Artist
     song.artist = self
   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+  def self.all
+    @@artists
   end
 end
+
